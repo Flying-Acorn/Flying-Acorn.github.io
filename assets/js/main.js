633b9acdf277030fbc7df45e2447adde8ad5db57
Amir Hakimnejad <amirhhakimnejad@gmail.com>
2024-07-01 02:53:34 +0330

    Add initial version of website

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function toggleNavbar() {
+    const navbar = document.querySelector('nav ul');
+    navbar.classList.toggle('active');
+}
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        navbar.classList.add('transparent');
+    } else {
+        navbar.classList.remove('transparent');
+    }
+});
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll);
